c215bb1ef9edcb4979bcc1bd0cec470846069f53
Brandon Gill <blg238@msstate.edu>
2018-09-11 11:11:48 -0500

    added to camera support

--- camera.py
+++ camera.py
@@ -2,24 +2,20 @@ import numpy as np
 import cv2 as cv
 
 
-
-
 cap = cv.VideoCapture(0)
-# Define the codec and create VideoWriter object
-fourcc = cv.VideoWriter_fourcc(*'XVID')
-out = cv.VideoWriter('output.avi',fourcc, 20.0, (640,480))
-while(cap.isOpened()):
+cap2 = cv.VideoCapture(1)
+while(True):
+    # Capture frame-by-frame
     ret, frame = cap.read()
-    if ret==True:
-        frame = cv.flip(frame,0)
-        # write the flipped frame
-        out.write(frame)
-        cv.imshow('frame',frame)
-        if cv.waitKey(1) & 0xFF == ord('q'):
-            break
-    else:
+    ret2, frame2 = cap2.read()
+    # Our operations on the frame come here
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    
+    # Display the resulting frame
+    cv.imshow('frame',gray)
+    cv.imshow('frame2', frame2)
+    if cv.waitKey(1) & 0xFF == ord('q'):
         break
-# Release everything if job is finished
+# When everything done, release the capture
 cap.release()
-out.release()
 cv.destroyAllWindows()
