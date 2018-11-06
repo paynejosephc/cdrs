1294408298bcc6077fc6e52c7f788be1d58b7825
Joseph Payne <paynejosephc@gmail.com>
2018-11-06 10:59:41 -0600

    added accel and debug

--- main.py
+++ main.py
@@ -2,22 +2,35 @@
 
 """ This is the main file for the Wrech Tech Team's Crash Data Recording System
 (CDRS) unit. This file will be run as a service on startup of the RPi """
-
+debug = True
+def debug(debugtext):
+    if debug:
+        print(debugtext)
+        
 import time
+from mpu6050 import Accel
 
 # Start up OBDII
+debug("Starting OBDII interface...")
 
 # Start up LiDAR 0
+debug("Starting front lidar interface...")
 
 # Start up LiDAR 1
+debug("Starting rear lidar interface...")
 
 # Start up camera 0
+debug("Starting front camera interface...")
 
 # Start up camera 1
+debug("Starting rear camera interface...")
 
 # Start up GPS
+debug("Starting GPS inteface...")
 
 # Start up the Accelerometer code
+debug("Starting Accelerometer interface...")
+acc = Accel()
 
 impact_magnitude = 0
 
@@ -26,18 +39,25 @@ while(True):
 
     if impact_magnitude >= 3.0:
         # trigger Camera 0
+        debug("TRIGGER: front camera")
 
         # trigger Camera 1
+        debug("TRIGGER: rear camera")
 
         # trigger LiDAR 0
+        debug("TRIGGER: front lidar")
 
         # trigger LiDAR 1
+        debug("TRIGGER: rear lidar")
 
         # trigger OBDII
+        debug("TRIGGER: OBDII")
 
         # trigger GPS
+        debug("TRIGGER: GPS")
 
         time.sleep(15)
+        debug("Waiting 15 for sensor data to stop writing to file")
 
 
         
